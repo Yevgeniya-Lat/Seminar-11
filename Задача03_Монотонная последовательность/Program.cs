90c9e941ea23ca8dec5222c987441f60239f7ff6
Yevgeniya-Lat <e9618281262@gmail.com>
2022-04-22 13:16:31 +0300

    Монотонная последовательность

---
+++ Задача03_Монотонная последовательность/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 1: 
+Есть монотонная последовательность, каждое число встречается ровно то количество раз, 
+какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... 
+Выведите ряд этой последовательности до N-го значения, желательно использовать :
+
+N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+*/
+
+
+int Prompt(string massage)
+{
+    System.Console.WriteLine(massage);
+    int Value = Convert.ToInt32(Console.ReadLine());
+    return Value;
+}
+
+
+void PrintNumber (int number, int k = 1, int m = 0)
+{
+    for ( int i = 0; i < k; i++)
+    {
+        System.Console.Write(k + " ");
+        m++;
+        if (m == number)
+        return;
+    }
+    PrintNumber(number, k + 1, m);
+}
+
+int N = Prompt("Введите N:  ");
+System.Console.WriteLine();
+PrintNumber(N);
