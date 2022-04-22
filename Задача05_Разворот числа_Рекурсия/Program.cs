bd2db233ee68bea1f329c9c6de76aea55c38ed5f
Yevgeniya-Lat <e9618281262@gmail.com>
2022-04-22 13:17:59 +0300

    Разворот числа Рекурсия

---
+++ Задача05_Разворот числа_Рекурсия/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 4: 
+Разверните число при помощи рекурсии:
+
+N = 1234 -> "4321"
+
+*/
+
+
+int Prompt(string massage)
+{
+    System.Console.WriteLine(massage);
+    int Value = Convert.ToInt32(Console.ReadLine());
+    return Value;
+}
+
+void Reverse(int number)
+        {
+            int newNum = number % 10;
+            Console.Write(newNum);
+            number /= 10;
+ 
+            if (number > 0)
+                Reverse(number);
+        }
+
+
+int N = Prompt("Введите N:  ");
+System.Console.WriteLine();
+Reverse(N);
